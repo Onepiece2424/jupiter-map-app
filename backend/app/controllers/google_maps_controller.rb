a6fabd46c136f2674e1b03fbe2b924c3c78897ac
rioto.nagano <kingdom950927@gmail.com>
2024-09-08 12:30:55 +0900

    feat:rails APIで検索フォームに入力したキーワードの緯度と経度を取得し、JSON形式でレスポンスを返す

---
+++ backend/app/controllers/google_maps_controller.rb
@@ -0,0 +1,11 @@
+class GoogleMapsController < ApplicationController
+  def search_location
+    results = Geocoder.search(params[:place_name])
+    if results.present?
+      location = results.first.coordinates
+      render json: { lat: location[0], lng: location[1] }
+    else
+      render json: { error: '位置が見つかりませんでした' }, status: :not_found
+    end
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :users
+  get 'search_location', to: 'google_maps#search_location'
 end
