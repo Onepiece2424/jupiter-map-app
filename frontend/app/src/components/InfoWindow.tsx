bddbb9efb1603cc7001ba696f4fbeff669763d86
rioto.nagano <kingdom950927@gmail.com>
2024-09-14 10:15:02 +0900

    fix:座標データが取得できないとき、エラーにならないように修正

--- frontend/app/src/components/InfoWindow.tsx
+++ frontend/app/src/components/InfoWindow.tsx
@@ -18,9 +18,9 @@ const InfoWindow: React.FC<InfoWindowProps> = ({ position, address }) => {
     <div>
       <p>緯度: {position.lat}</p>
       <p>経度: {position.lng}</p>
-      <p>国: {address.country}</p>
-      <p>郵便番号: {address.postcode}</p>
-      <p>市区町村: {address.city}</p>
+      <p>国: {address?.country}</p>
+      <p>郵便番号: {address?.postcode}</p>
+      <p>市区町村: {address?.city}</p>
     </div>
   );
 };
