f7f6ed880fd2bbf19661946f222295e22419bace
rioto.nagano <kingdom950927@gmail.com>
2024-09-22 05:48:25 +0900

    feat:devise-token-authの導入

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   resources :users, :favorite_places
   get 'search_location', to: 'google_maps#search_location'
   get 'reverse_geocode', to: 'google_maps#reverse_geocode'
