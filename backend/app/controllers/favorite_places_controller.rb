4da078f2dd190df52f68eaf8e41d915cb36ddc19
rioto.nagano <kingdom950927@gmail.com>
2024-09-21 20:20:45 +0900

    feat:favorite_placesのindex,newアクションの作成

--- backend/app/controllers/favorite_places_controller.rb
+++ backend/app/controllers/favorite_places_controller.rb
@@ -1,2 +1,19 @@
 class FavoritePlacesController < ApplicationController
+
+  def index
+    @favorite_places = FavoritePlace.all
+  end
+
+  def new
+    @favorite_place = FavoritePlace.new
+  end
+
+  def create
+  end
+
+  private
+
+  def favorite_places_params
+    params.require(:favorite_places).permit(:latitude, :longitude, :place_name, :country, :postcode)
+  end
 end
