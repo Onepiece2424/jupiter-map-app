720425031a76059a62b44e06ee3cadff5f3370d2
rioto.nagano <kingdom950927@gmail.com>
2024-09-21 20:33:41 +0900

    feat:「はい」ボタンを押下したとき、コンソールにピンのデータ表示

--- frontend/app/src/components/InfoWindow.tsx
+++ frontend/app/src/components/InfoWindow.tsx
@@ -4,6 +4,15 @@ const InfoWindow = ({ position, address }: InfoWindowProps) => {
   const { lat, lng } = position;
   const { country = "不明", postcode = "不明", city = "不明" } = address || {};
 
+  // お気に入り登録
+  const handleClick = () => {
+    createFavoritePlace(lat, lng, country, postcode, city);
+  };
+
+  const createFavoritePlace = (lat: number, lng: number, country: string, postcode: string, city: string) => {
+    console.log(lat, lng, country, postcode, city);
+  }
+
   return (
     <div>
       <p>緯度: {lat}</p>
@@ -11,6 +20,11 @@ const InfoWindow = ({ position, address }: InfoWindowProps) => {
       <p>国: {country}</p>
       <p>郵便番号: {postcode}</p>
       <p>市区町村: {city}</p>
+      <p>お気に入り登録しますか？</p>
+      <div>
+        <button onClick={handleClick}>はい</button>
+        <button>いいえ</button>
+      </div>
     </div>
   );
 }
