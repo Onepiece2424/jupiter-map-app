813994a9ebf4725d3b94e1ada5d49799f292dc6c
rioto.nagano <kingdom950927@gmail.com>
2024-09-22 19:56:27 +0900

    fix:password_digestカラムの削除

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_21_204742) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_22_105131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,7 +32,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_21_204742) do
     t.integer "age"
     t.integer "gender"
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "provider", default: "email", null: false
