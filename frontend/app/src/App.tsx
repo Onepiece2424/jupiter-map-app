b528321fdb93385d9a83eeae0c19c7f3976adccf
rioto.nagano <kingdom950927@gmail.com>
2024-09-25 21:11:02 +0900

    feat:AppコンポーネントにRecoilRootを定義し、どのコンポーネントでもRecoilを使用できるようにする

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,20 +1,23 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import GoogleMaps from './components/GoogleMaps';
 import UserData from './components/UserData';
 import LoginForm from './components/LoginForm';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/users" element={<UserData />} />
-          <Route path="/" element={<GoogleMaps />} />
-          <Route path="/login" element={<LoginForm />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/users" element={<UserData />} />
+            <Route path="/" element={<GoogleMaps />} />
+            <Route path="/login" element={<LoginForm />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
 
