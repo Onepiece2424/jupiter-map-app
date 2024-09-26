2f5799ac88b8d917fcf8ae19af75d5a4e8293bbd
rioto.nagano <kingdom950927@gmail.com>
2024-09-26 18:24:24 +0900

    feat:複雑なデータ構造の時の分割代入で変数名の変更

--- frontend/app/src/components/TsPractice.tsx
+++ frontend/app/src/components/TsPractice.tsx
@@ -2,10 +2,22 @@ import React from 'react'
 
 const TsPractice = () => {
 
-  // 分割代入で名前の変更
+  // 分割代入で変数名の変更
   const obj = { fruit: "apple", age: 18, flag: true }
   const { fruit, age: animalAge } = obj
   console.log(fruit, animalAge); // => "apple" 18
+
+  // 複雑なデータ構造の時の分割代入で変数名の変更
+  const user = {
+    id: 1,
+    profile: {
+      name: "John Doe",
+      age: 30
+    }
+  };
+
+  const { profile: { name: userName, age: userAge } } = user;
+  console.log(userName, userAge); // => "John Doe", 30
 
   return (
     <div>TsPractice</div>
