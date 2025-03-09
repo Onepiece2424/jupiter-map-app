434b9ea04b9741d104aa47affbb619f78811e9b8
rioto.nagano <kingdom950927@gmail.com>
2025-03-09 10:55:56 +0900

    refac: 開始位置を揃える

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -10,8 +10,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "localhost:3001"
 
     resource "*",
-      headers: :any,
-      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+             headers: :any,
+             expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+             methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
