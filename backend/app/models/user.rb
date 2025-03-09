79242fd052bc8c9dac50202a4fd95e6f800e5a7a
rioto.nagano <kingdom950927@gmail.com>
2025-03-09 10:55:13 +0900

    refac: 開始位置を揃える

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   extend Devise::Models
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable, :omniauthable
+         :recoverable, :rememberable, :validatable, :omniauthable
   include DeviseTokenAuth::Concerns::User
 
   has_many :favorite_places
