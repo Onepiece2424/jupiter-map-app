d41a95ea3d69d1c5a6c210817218da33db9d6553
rioto.nagano <kingdom950927@gmail.com>
2025-03-15 10:28:09 +0900

    feat: 詳細ページ、編集ページ、お気に入り場所のデータ更新のAPIの作成

--- backend/app/controllers/favorite_places_controller.rb
+++ backend/app/controllers/favorite_places_controller.rb
@@ -1,9 +1,10 @@
 class FavoritePlacesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_favorite_place, only: %i[show edit update]
 
   def index
     @favorite_places = current_user.favorite_places
-                       .map { |p| { id: p.id, lat: p.latitude, lng: p.longitude, place_name: p.place_name } }
+                                   .map { |p| { id: p.id, lat: p.latitude, lng: p.longitude, place_name: p.place_name } }
     render json: @favorite_places
   end
 
@@ -16,9 +17,28 @@ class FavoritePlacesController < ApplicationController
     render json: @favorite_place
   end
 
+  def show
+    @favorite_place = FavoritePlace.find(params[:id])
+    render json: @favorite_place.as_json(methods: :image_url)
+  end
+
+  def edit; end
+
+  def update
+    if @favorite_place.update!(favorite_places_params)
+      render json: @favorite_place, status: :ok
+    else
+      render json: { errors: @favorite_place.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def favorite_places_params
-    params.require(:favorite_places).permit(:latitude, :longitude, :place_name, :country, :postcode)
+    params.require(:favorite_places).permit(:latitude, :longitude, :place_name, :country, :postcode, :description, :image)
+  end
+
+  def set_favorite_place
+    @favorite_place = FavoritePlace.find(params[:id])
   end
 end
