c6a7a7f6a42c3acf3239a77db071e8e7d2a26718
rioto.nagano <kingdom950927@gmail.com>
2025-03-20 16:08:33 +0900

    feat: 場所画像を登録していないとき、NoImgae画像を表示

--- backend/app/models/favorite_place.rb
+++ backend/app/models/favorite_place.rb
@@ -1,10 +1,12 @@
 class FavoritePlace < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   belongs_to :user
   has_one_attached :image do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
   end
 
   def image_url
-    image.attached? ? Rails.application.routes.url_helpers.url_for(image) : nil
+    image.attached? ? url_for(image) : nil
   end
 end
