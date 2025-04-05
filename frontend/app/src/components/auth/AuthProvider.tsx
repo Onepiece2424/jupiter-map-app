91877291e8d9658c4d9978afe6b4298357e506bf
rioto.nagano <kingdom950927@gmail.com>
2025-04-05 21:08:43 +0900

    refac: 認証部分をカスタムHooksに共通化

--- frontend/app/src/components/auth/AuthProvider.tsx
+++ frontend/app/src/components/auth/AuthProvider.tsx
@@ -36,10 +36,4 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   );
 };
 
-export const useAuth = () => {
-  const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error("useAuth must be used within an AuthProvider");
-  }
-  return context;
-};
+export default AuthContext;
--- frontend/app/src/components/auth/PrivateRoute.tsx
+++ frontend/app/src/components/auth/PrivateRoute.tsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "./AuthProvider";
+import { useAuth } from "../../hooks/useAuth";
 import { useRecoilValue } from "recoil";
 import { authState } from "../../atoms/auth";
 
