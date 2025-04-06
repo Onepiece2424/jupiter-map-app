05afac5cdb5f7c13e302941e741146248246daa6
rioto.nagano <kingdom950927@gmail.com>
2025-04-06 20:05:03 +0900

    change: 友達関連付けデータを有する友達データを全て表示させる

--- backend/app/controllers/friends_controller.rb
+++ backend/app/controllers/friends_controller.rb
@@ -2,7 +2,7 @@ class FriendsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @friends = current_user.friends
+    @friends = current_user.all_friends
     render json: @friends
   end
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :favorite_places
   has_many :friendships
   has_many :friends, through: :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
   has_many :sent_friend_requests, class_name: 'FriendRequest', foreign_key: 'sender_id', dependent: :destroy
   has_many :received_friend_requests, class_name: 'FriendRequest', foreign_key: 'receiver_id', dependent: :destroy
 
@@ -18,4 +20,8 @@ class User < ApplicationRecord
   # validates :password, presence: true
 
   enum gender: { male: 1, female: 2, other: 3 }
+
+  def all_friends
+    (friends + inverse_friends).uniq { |friend| friend.id }
+  end
 end
