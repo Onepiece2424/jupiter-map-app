515cac56f7de35098899906c25833f5fdc882dca
rioto.nagano <kingdom950927@gmail.com>
2025-04-06 13:49:40 +0900

    refac: 不要な空白を削除

--- backend/db/migrate/20250405225441_create_friendships.rb
+++ backend/db/migrate/20250405225441_create_friendships.rb
@@ -1,13 +1,13 @@
 class CreateFriendships < ActiveRecord::Migration[7.0]
   def change
     create_table :friendships do |t|
-      t.references :user, null: false, foreign_key: { to_table: :users }  # ユーザー
-      t.references :friend, null: false, foreign_key: { to_table: :users }  # 友達
+      t.references :user, null: false, foreign_key: { to_table: :users } # ユーザー
+      t.references :friend, null: false, foreign_key: { to_table: :users } # 友達
 
       t.timestamps
     end
 
-    add_index :friendships, [:user_id, :friend_id], unique: true  # 重複する友達関係を防ぐ
-    add_index :friendships, [:friend_id, :user_id]  # 双方向検索を効率化
+    add_index :friendships, [:user_id, :friend_id], unique: true # 重複する友達関係を防ぐ
+    add_index :friendships, [:friend_id, :user_id] # 双方向検索を効率化
   end
 end
